3cb0030c6c3110c641740bc2219f91292972aca5
Dima Lavrov <aston007d@yandex.ru>
2022-11-25 02:41:44 +0600

    Урок 2. Базовые алгоритмы Задача 10

---
+++ Task10/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+string? a = "";
+Console.WriteLine("Задача 10 \r\n");
+Regex regex = new Regex(@"^\d{3}$");
+
+do
+{
+    Console.WriteLine("Введите 3 значное число. Цифры от 0 - 9");
+    a = Console.ReadLine();
+
+} while (a == null || !regex.Match(a).Success);
+
+Console.WriteLine(a[1]);
+Console.ReadLine();
