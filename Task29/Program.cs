342fcbf3f8de121b546d8b3a4580b51d669799b9
Dima Lavrov <aston007d@yandex.ru>
2022-11-25 03:45:37 +0600

    Задача 29

---
+++ Task29/Program.cs
@@ -0,0 +1,19 @@
+﻿int value;
+bool res;
+string? input;
+List<int> list = new List<int>();
+Console.WriteLine("Введите целочисленные значения подряд. Для завершение набрать 'stop'");
+
+do
+{
+    input = Console.ReadLine();
+    res = int.TryParse(input, out value);
+    if (res)
+    {
+        list.Add(value);
+    }   
+    
+} while (input != "stop" && input != null);
+
+Console.WriteLine(string.Join(", ", list));
+Console.ReadLine();
