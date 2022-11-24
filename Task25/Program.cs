af86eb3397034cbe71ea397f9198aec9ed5fa311
Dima Lavrov <aston007d@yandex.ru>
2022-11-25 03:01:58 +0600

    Знакомство с языками программирования Урок 4. Функции

---
+++ Task25/Program.cs
@@ -0,0 +1,22 @@
+﻿int A, B;
+Console.WriteLine("Задача 25 \r\nВведите число A и B:");
+bool resultA = int.TryParse(Console.ReadLine(), out A);
+bool resultB = int.TryParse(Console.ReadLine(), out B);
+
+
+if (resultA && resultB)
+{
+    Console.WriteLine(Pow(A, B));
+    Console.ReadLine();
+}
+else
+{
+    Console.WriteLine("Некорректный ввод");
+}
+
+static int Pow(int number, int power)
+{
+    if (power == 1)
+        return number;
+    return number * Pow(number, --power);
+}
