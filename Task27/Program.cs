d7c1a431263764ddc59029389e64ce744009194a
Dima Lavrov <aston007d@yandex.ru>
2022-11-25 03:19:00 +0600

    Задача 27

---
+++ Task27/Program.cs
@@ -0,0 +1,21 @@
+﻿int A;
+Console.WriteLine("Задача 27 \r\nВведите число");
+bool resultA = int.TryParse(Console.ReadLine(), out A);
+
+
+
+if (resultA)
+{
+    Console.WriteLine(Sum(A));
+    Console.ReadLine();
+}
+else
+{
+    Console.WriteLine("Некорректный ввод");
+}
+
+static int Sum(int number)
+{
+    return number.ToString().Select(c => c.ToString()).Sum(x => Convert.ToInt32(x));
+}
+    
