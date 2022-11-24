93d0533e2ef42c7e2e8f0effd5e32af664377335
Dima Lavrov <aston007d@yandex.ru>
2022-11-25 02:46:41 +0600

    Урок 2. Базовые алгоритмы Задача 13

---
+++ Task13/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+string? a = "";
+Console.WriteLine("Задача 13 \r\n");
+Regex regex = new Regex(@"^\d+$");
+
+do
+{
+    Console.WriteLine("Введите число. Цифры от 0 - 9");
+    a = Console.ReadLine();
+
+} while (a == null || !regex.Match(a).Success);
+
+if (a.Length > 3)
+{
+    Console.WriteLine(a[2]);
+}
+else
+    Console.WriteLine("третьей цифры нет");
+Console.ReadLine();
