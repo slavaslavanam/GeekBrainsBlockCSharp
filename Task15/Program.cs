df97ddecd484fcd7e6888e21b6651b9e7ef45ce0
Dima Lavrov <aston007d@yandex.ru>
2022-11-25 02:53:39 +0600

    Урок 2. Базовые алгоритмы Задача 15

---
+++ Task15/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+string? a = "";
+Console.WriteLine("Задача 15 \r\n");
+Regex regex = new Regex(@"^[1-7]{1}$");
+
+do
+{
+    Console.WriteLine("Введите число. Цифра от 0 - 9");
+    a = Console.ReadLine();
+
+} while (a == null || !regex.Match(a).Success);
+
+if (Convert.ToDouble(a) > 5)
+{
+    Console.WriteLine("да");
+}
+else
+    Console.WriteLine("нет");
+Console.ReadLine();
