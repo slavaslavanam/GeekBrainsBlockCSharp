7041013728229255b68ff15f51f12a8eedc7f16f
Dima Lavrov <aston007d@yandex.ru>
2022-12-17 22:46:32 +0600

    Task66

---
+++ Task66/Program.cs
@@ -0,0 +1,16 @@
+﻿int n = 0;
+int m = 0;
+Console.WriteLine("Задача 66");
+Console.WriteLine("Введите целое число m:");
+Int32.TryParse(Console.ReadLine(), out m);
+Console.WriteLine("Введите целое число n:");
+Int32.TryParse(Console.ReadLine(), out n);
+Console.WriteLine($"sum = {SummN(m, n)}");
+
+static int SummN(int m, int n)
+{
+    if (m == n)
+        return n;
+    else
+        return m + SummN(m + 1,n);
+}
