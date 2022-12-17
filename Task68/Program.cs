c1633ce24167ca240584e19fea7c8840ad7c2255
Dima Lavrov <aston007d@yandex.ru>
2022-12-17 22:50:41 +0600

    Task68

---
+++ Task68/Program.cs
@@ -0,0 +1,20 @@
+﻿uint n = 0;
+uint m = 0;
+Console.WriteLine("Задача 66");
+Console.WriteLine("Введите целое число m:");
+UInt32.TryParse(Console.ReadLine(), out m);
+Console.WriteLine("Введите целое число n:");
+UInt32.TryParse(Console.ReadLine(), out n);
+Console.WriteLine();
+Console.WriteLine("F() = " + Akk(m, n));
+
+static uint Akk(uint n, uint m)
+{
+    if (n == 0)
+        return m + 1;
+    else
+      if ((n != 0) && (m == 0))
+        return Akk(n - 1, 1);
+    else
+        return Akk(n - 1, Akk(n, m - 1));
+}
