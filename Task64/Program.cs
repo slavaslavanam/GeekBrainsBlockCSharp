1c286eb8fc7218ccf8209aaed999d2eb8bd5a4f0
Dima Lavrov <aston007d@yandex.ru>
2022-12-17 22:27:49 +0600

    Task64

---
+++ Task64/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Задача 64");
+Console.WriteLine("Введите целое число n:");
+int n = 0;
+Int32.TryParse(Console.ReadLine(), out n);
+Recur(n);
+
+ static void Recur(int n)
+{
+    if (n == 1) Console.Write(n + ", ");
+    else
+    {
+        Console.Write(n + ", ");
+        Recur(n - 1);
+    }
+}
